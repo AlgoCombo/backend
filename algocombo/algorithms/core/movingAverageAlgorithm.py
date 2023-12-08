e472b4b574680032e0a0574cb53a8f30d21e2fbf
bamblebam <burhanizrangwala@gmail.com>
2023-12-09 01:26:20 +0530

    feat: relative strenfth index algorithm

--- algocombo/algorithms/core/movingAverageAlgorithm.py
+++ algocombo/algorithms/core/movingAverageAlgorithm.py
@@ -13,7 +13,7 @@ class MovingAverageAlgorithm(BaseAlgorithm):
         df = pd.DataFrame(self._inputs, columns=['timestamp', 'price'])
         return df
 
-    def get_signal(self, short_window=20, long_window=50, day_number=101):
+    def get_signal(self, short_window=20, long_window=50, *args, **kwargs):
         """
         1=buy, 
         0=hold, 
@@ -21,35 +21,24 @@ class MovingAverageAlgorithm(BaseAlgorithm):
 
         @param short_window: int
         @param long_window: int
-        @param day_number: int the day for which the signal is required .ie. for the 101st day, day_number=101
-        @return: int the buy/sell signal for the day_number'th day
-
-        given day is the day before the required day to get the signal for
+        @return: int the buy/sell signal for the latest available day
         """
+        print(short_window, long_window)
         data = self.clean_data()
         data['Short_MA'] = data['price'].rolling(
             window=short_window, min_periods=1).mean()
         data['Long_MA'] = data['price'].rolling(
             window=long_window, min_periods=1).mean()
 
-        # Generate buy/sell signals based on crossover
-        data['Signal'] = 0
-        data.loc[data['Short_MA'] > data['Long_MA'], 'Signal'] = 1
-        data.loc[data['Short_MA'] < data['Long_MA'], 'Signal'] = -1
+        # Generate buy/sell signals based on crossover for the latest day
+        signal_latest_day = 0  # Initialize the signal for the latest day
 
-        # Extract the moving averages for the 101st day
-        short_ma_given_day = data.iloc[day_number-2]['Short_MA']
-        long_ma_given_day = data.iloc[day_number-2]['Long_MA']
+        if data['Short_MA'].iloc[-1] > data['Long_MA'].iloc[-1]:
+            signal_latest_day = 1  # Buy signal
+        elif data['Short_MA'].iloc[-1] < data['Long_MA'].iloc[-1]:
+            signal_latest_day = -1  # Sell signal
 
-        # Use the moving averages from the given day to predict the signal for the 101st day
-        signal_required_day = 0  # Initialize the signal for the required day
-
-        if short_ma_given_day > long_ma_given_day:
-            signal_required_day = 1
-        elif short_ma_given_day < long_ma_given_day:
-            signal_required_day = -1
-
-        return signal_required_day
+        return signal_latest_day
 
     def __str__(self):
         return self._name
--- algocombo/algorithms/routers.py
+++ algocombo/algorithms/routers.py
@@ -3,13 +3,16 @@ from fastapi import APIRouter, Body, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from algorithms.core.movingAverageAlgorithm import MovingAverageAlgorithm
+from algorithms.core.relativeStrengthIndexAlgorithm import RelativeStrengthIndexAlgorithm
 
 router = APIRouter()
 
 
 class get_signal_request(BaseModel):
     inputs: List[List[Union[str, float, int]]]
-    timeframe: str = "day"
+    timeframe: str = "day",
+    args: List = []
+    kwargs: dict = {}
 
 
 @router.post("/get_signal/{algorithm_name}", tags=["algorithms"])
@@ -18,12 +21,16 @@ async def get_signal(algorithm_name: str, request_body: get_signal_request = Bod
     Get the signal for the given algorithm
     """
     algorithm_object = None
-    if algorithm_name == "Moving Average Algorithm":
+    if algorithm_name == "MovingAverageAlgorithm":
         algorithm_object = MovingAverageAlgorithm(
+            request_body.inputs, request_body.timeframe)
+    elif algorithm_name == "RelativeStrengthIndexAlgorithm":
+        algorithm_object = RelativeStrengthIndexAlgorithm(
             request_body.inputs, request_body.timeframe)
     else:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": "Invalid algorithm name"})
 
-    signal = algorithm_object.get_signal()
+    signal = algorithm_object.get_signal(
+        *request_body.args, **request_body.kwargs)
 
     return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Signal retreived successfully", "signal": signal})
