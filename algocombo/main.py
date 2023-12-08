70af7ccbab2864537c9324f4e68ea50fd5318eaf
bamblebam <burhanizrangwala@gmail.com>
2023-12-08 22:10:58 +0530

    chore: json response and health check

--- algocombo/main.py
+++ algocombo/main.py
@@ -1,15 +1,12 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, status
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def health_check():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message": "Hello World"})
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
